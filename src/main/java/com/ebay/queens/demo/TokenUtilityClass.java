fe6a05826ac389847cb18fe9eae3e3bf0e1e2562
EdwardMuldrew <emuldrew03@aub.ac.uk>
2020-01-08 13:50:30 +0000

    Add token utility class

---
+++ src/main/java/com/ebay/queens/demo/TokenUtilityClass.java
@@ -0,0 +1,31 @@
+package com.ebay.queens.demo;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class TokenUtilityClass {
+	
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Paypal.class);
+	
+	 @GET
+	    @Path("/authenticationtoken")
+	    @Produces(MediaType.APPLICATION_JSON)
+	    @RequestMapping(value = "/patientdetails", method = RequestMethod.POST,consumes = MediaType.APPLICATION_FORM_URLENCODED)
+	    public String authenticationToken() throws IOException {
+	    	logger.info("Authentication Token");
+	    	String requestBody = ""; 
+	    	logger.info(requestBody);
+	    	String url = "https://api.paypal.com/v1/oauth2/token";
+	    	String response = Http.authenticationPost(url, requestBody, "PaypalAuth");
+	    	return response;
+	    }
+
+}
