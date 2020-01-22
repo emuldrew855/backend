650790396b48b397c56ae497c30f9c97b122e67b
Edward Muldrew <edwardmuldrew@gmail.com>
2020-01-22 13:52:18 +0000

    Add find non profit request

---
+++ src/main/java/com/ebay/queens/requests/FindNonProfitRequest.java
@@ -0,0 +1,34 @@
+package com.ebay.queens.requests;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "GetItemRequest")
+public class FindNonProfitRequest {
+	private SearchFilter searchFilter;
+	private PaginationInput paginationInput;
+	
+	public FindNonProfitRequest(SearchFilter searchFilter, PaginationInput paginationInput) {
+		this.searchFilter = searchFilter;
+		this.paginationInput = paginationInput;
+	}
+	
+	// Getters
+	public SearchFilter getSearchFilter() {
+		return searchFilter;
+	}
+
+	public PaginationInput getPaginationInput() {
+		return paginationInput;
+	}
+	
+	// Setters
+	public void setSearchFilter(SearchFilter searchFilter) {
+		this.searchFilter = searchFilter;
+	}
+
+	public void setPaginationInput(PaginationInput paginationInput) {
+		this.paginationInput = paginationInput;
+	}
+	
+	
+}
