a44fa17d9b770ad5c02ba269dd8e18160811c088
EdwardMuldrew <emuldrew03@aub.ac.uk>
2020-02-26 19:55:33 +0000

    Add Login api

---
+++ src/main/java/com/ebay/queens/demo/Login.java
@@ -0,0 +1,40 @@
+package com.ebay.queens.demo;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/auth")
+public class Login {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Login.class);
+	
+	Login() {
+		LOGGER.info("Login");
+	}
+	
+	@GET
+	@Path("/LogIn")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String logIn(@QueryParam("username") String username, @QueryParam("password") String password) { 
+		LOGGER.info("Log In Method" + username + " " + password);
+		String response = "";
+		if(username.equals("username") && password.equals("password")) {
+			response = "GrantAccess";
+		}else {
+			response = "NoAccess";
+		}
+		
+		return response;
+	}
+}
